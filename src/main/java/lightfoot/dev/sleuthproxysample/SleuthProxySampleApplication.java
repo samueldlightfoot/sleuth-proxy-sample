335ca0b8ed2d27ef1d56a1aeb1d8de8d94496864
samueldlightfoot <samueldlightfoot@gmail.com>
2022-06-25 14:45:53 +0100

    Initial commit

---
+++ src/main/java/lightfoot/dev/sleuthproxysample/SleuthProxySampleApplication.java
@@ -0,0 +1,13 @@
+package lightfoot.dev.sleuthproxysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthProxySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthProxySampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lightfoot/dev/sleuthproxysample/SleuthProxySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package lightfoot.dev.sleuthproxysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthProxySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
